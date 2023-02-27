fd68d10325b032f014ad32b9bfa3d533832828c4
Albert Isern Alvarez <albert.isern.alvarez@gmail.com>
2023-02-27 02:26:40 +0100

    Stylized

--- app.vue
+++ app.vue
@@ -2,7 +2,7 @@
   <div class="flex w-full justify-center gap-0 lg:gap-4">
     <HomeSidebar />
     <div
-      class="hover-animation flex min-h-screen w-full max-w-xl flex-col border-x-0 border-light-border pb-96 dark:border-dark-border xs:border-x">
+      class="hover-animation flex min-h-screen w-full max-w-xl flex-col border-x-0 border-dark-border pb-96 dark:border-light-border xs:border-l-2 sm:border-x-2">
       <NuxtLayout>
         <NuxtLoadingIndicator :height="5" :duration="3000" :throttle="400" />
         <NuxtPage />
