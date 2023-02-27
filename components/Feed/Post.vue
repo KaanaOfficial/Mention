96117ffe541beac69801017147656c0dad719bf2
Albert Isern Alvarez <albert.isern.alvarez@gmail.com>
2023-02-27 01:22:43 +0100

    New template

--- components/Feed/Post.vue
+++ components/Feed/Post.vue
@@ -5,12 +5,12 @@
       <div class="grid grid-cols-[auto,1fr] gap-x-3 gap-y-1">
         <div class="flex flex-col items-center gap-2">
           <div class="group relative self-start text-light-primary dark:text-dark-primary [&amp;>div]:translate-y-2"><a
-              class="blur-picture flex self-start" tabindex="0" href="/user/lolol">
-              <figure style="width: 48px;"><span
+              class="blur-picture flex self-start" to="/@{{ post.author.username}}">
+              <figure style="width: 48px;">
+                <span
                   style="box-sizing: border-box; display: block; overflow: hidden; width: initial; height: initial; background: none; opacity: 1; border: 0px; margin: 0px; padding: 0px; position: relative;"><span
                     style="box-sizing: border-box; display: block; width: initial; height: initial; background: none; opacity: 1; border: 0px; margin: 0px; padding: 100% 0px 0px;"></span><img
-                    alt="good looks" src="https://pbs.twimg.com/profile_images/1604802383162269698/MbMxGgB7_400x400.jpg"
-                    class="rounded-full object-cover" sizes="100vw"
+                    alt="{{ post.author.name}}" :src="post.author.avatar" class="rounded-full object-cover" sizes="100vw"
                     style="position: absolute; inset: 0px; box-sizing: border-box; padding: 0px; border: none; margin: auto; display: block; width: 0px; height: 0px; min-width: 100%; max-width: 100%; min-height: 100%; max-height: 100%;"></span>
               </figure>
             </a>
@@ -25,15 +25,16 @@
             <div class="flex gap-1 truncate xs:overflow-visible xs:whitespace-normal">
               <div
                 class="group relative self-start text-light-primary dark:text-dark-primary grid [&amp;>div]:translate-y-7">
-                <a class="flex items-center gap-1 truncate font-bold custom-underline text-light-primary dark:text-dark-primary"
-                  tabindex="0" href="/user/lolol">
-                  <p class="truncate">good looks</p><i><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"
-                      fill="currentColor" aria-hidden="true" class="fill-accent-blue h-5 w-5">
+                <NuxtLink
+                  class="flex items-center gap-1 truncate font-bold custom-underline text-light-primary dark:text-dark-primary"
+                  to="/@{{ post.author.username}}">
+                  <p class="truncate">{{ post.author.name }}</p><i><svg xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 24 24" fill="currentColor" aria-hidden="true" class="fill-accent-blue h-5 w-5">
                       <path fill-rule="evenodd"
                         d="M8.603 3.799A4.49 4.49 0 0112 2.25c1.357 0 2.573.6 3.397 1.549a4.49 4.49 0 013.498 1.307 4.491 4.491 0 011.307 3.497A4.49 4.49 0 0121.75 12a4.49 4.49 0 01-1.549 3.397 4.491 4.491 0 01-1.307 3.497 4.491 4.491 0 01-3.497 1.307A4.49 4.49 0 0112 21.75a4.49 4.49 0 01-3.397-1.549 4.49 4.49 0 01-3.498-1.306 4.491 4.491 0 01-1.307-3.498A4.49 4.49 0 012.25 12c0-1.357.6-2.573 1.549-3.397a4.49 4.49 0 011.307-3.497 4.49 4.49 0 013.497-1.307zm7.007 6.387a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z"
                         clip-rule="evenodd"></path>
                     </svg></i>
-                </a>
+                </NuxtLink>
                 <div
                   class="menu-container invisible absolute left-1/2 w-72 -translate-x-1/2 rounded-2xl opacity-0 [transition:visibility_0ms_ease_400ms,opacity_200ms_ease_200ms] group-hover:visible group-hover:opacity-100 group-hover:delay-500">
                   <MiniCardsUser />
@@ -41,16 +42,17 @@
               </div>
               <div
                 class="group relative self-start text-light-primary dark:text-dark-primary grid [&amp;>div]:translate-y-7">
-                <a class="truncate text-light-secondary dark:text-dark-secondary" tabindex="-1"
-                  href="/user/lolol">@lolol</a>
+                <NuxtLink class="truncate text-light-secondary dark:text-dark-secondary" tabindex="-1"
+                  to="/@{{ post.author.username}}">
+                  @{{ post.author.username }}</NuxtLink>
                 <div
                   class="menu-container invisible absolute left-1/2 w-72 -translate-x-1/2 rounded-2xl opacity-0 [transition:visibility_0ms_ease_400ms,opacity_200ms_ease_200ms] group-hover:visible group-hover:opacity-100 group-hover:delay-500">
                   <MiniCardsUser />
                 </div>
               </div>
               <div class="flex gap-1"><i>·</i>
-                <div class="group relative"><a class="custom-underline peer whitespace-nowrap"
-                    href="/tweet/QDvtIWZ0ERT3o8oS6M6M">23 Feb</a>
+                <div class="group relative">
+                  <NuxtLink class="custom-underline peer whitespace-nowrap" to="/post/{{ post.id }}">23 Feb</NuxtLink>
                   <div
                     class="invisible absolute left-1/2 -translate-x-1/2 whitespace-nowrap rounded bg-[#666666] px-1 py-0.5 text-xs text-white opacity-0 [transition:visibility_0ms_ease_200ms,opacity_200ms_ease] dark:bg-[#495A69] group-hover:visible group-hover:opacity-100 group-hover:delay-500 group-focus-visible:visible group-focus-visible:opacity-100 translate-y-1 peer-focus:opacity-100 peer-focus-visible:visible peer-focus-visible:delay-200">
                     <span>16:31 · 23 Feb 2023</span>
@@ -59,11 +61,13 @@
               </div>
             </div>
             <div class="px-4">
-              <div data-headlessui-state=""><button
+              <div data-headlessui-state="">
+                <button
                   class="custom-button main-tab main-tab group group absolute top-2 right-2 p-2 hover:bg-accent-blue/10 focus-visible:bg-accent-blue/10 focus-visible:!ring-accent-blue/80 active:bg-accent-blue/20"
                   id="headlessui-popover-button-:r3p:" aria-expanded="false" data-headlessui-state="" type="button">
-                  <div class="group relative"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                      stroke-width="1.5" stroke="currentColor" aria-hidden="true"
+                  <div class="group relative">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                      stroke="currentColor" aria-hidden="true"
                       class="h-5 w-5 text-light-secondary group-hover:text-accent-blue group-focus-visible:text-accent-blue dark:text-dark-secondary/80">
                       <path stroke-linecap="round" stroke-linejoin="round"
                         d="M6.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM12.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM18.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0z">
@@ -74,22 +78,17 @@
                       <span>More</span>
                     </div>
                   </div>
-                </button></div>
+                </button>
+              </div>
             </div>
           </div>
-          <p class="whitespace-pre-line break-words">{{ post.title }}</p>
+          <p class="whitespace-pre-line break-words">{{ post.content }}</p>
           <div class="mt-1 flex flex-col gap-2">
             <div class="flex text-light-secondary inner:outline-none dark:text-dark-secondary max-w-md justify-between">
               <button
-                class="group flex items-center gap-1.5 p-0 transition-none
-                                                                                               disabled:cursor-not-allowed inner:transition inner:duration-200 hover:text-accent-blue focus-visible:text-accent-blue"><i
-                  class="relative rounded-full p-2 not-italic group-focus-visible:ring-2 group-hover:bg-accent-blue/10 group-active:bg-accent-blue/20 group-focus-visible:bg-accent-blue/10 group-focus-visible:ring-accent-blue/80"><svg
-                    xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                    stroke="currentColor" aria-hidden="true" class="h-5 w-5">
-                    <path stroke-linecap="round" stroke-linejoin="round"
-                      d="M12 20.25c4.97 0 9-3.694 9-8.25s-4.03-8.25-9-8.25S3 7.444 3 12c0 2.104.859 4.023 2.273 5.48.432.447.74 1.04.586 1.641a4.483 4.483 0 01-.923 1.785A5.969 5.969 0 006 21c1.282 0 2.47-.402 3.445-1.087.81.22 1.668.337 2.555.337z">
-                    </path>
-                  </svg>
+                class="group flex items-center gap-1.5 p-0 transition-none disabled:cursor-not-allowed inner:transition inner:duration-200 hover:text-accent-blue focus-visible:text-accent-blue"><i
+                  class="relative rounded-full p-2 pt-1 not-italic group-focus-visible:ring-2 group-hover:bg-accent-blue/10 group-active:bg-accent-blue/20 group-focus-visible:bg-accent-blue/10 group-focus-visible:ring-accent-blue/80">
+                  <Icon name="icon-park-twotone:comments" size="18" />
                   <div
                     class="invisible absolute left-1/2 -translate-x-1/2 whitespace-nowrap rounded bg-[#666666] px-1 py-0.5 text-xs text-white opacity-0 [transition:visibility_0ms_ease_200ms,opacity_200ms_ease] dark:bg-[#495A69] group-hover:visible group-hover:opacity-100 group-hover:delay-500 group-focus-visible:visible group-focus-visible:opacity-100 translate-y-3">
                     <span>Reply</span>
@@ -98,19 +97,14 @@
                 <div class="overflow-hidden">
                   <p class="text-sm" style="opacity: 1; transform: none;">1</p>
                 </div>
-              </button><button
-                class="group flex items-center gap-1.5 p-0 transition-none
-                                                                                               disabled:cursor-not-allowed inner:transition inner:duration-200 hover:text-accent-green focus-visible:text-accent-green"><i
-                  class="relative rounded-full p-2 not-italic group-focus-visible:ring-2 group-hover:bg-accent-green/10 group-active:bg-accent-green/20 group-focus-visible:bg-accent-green/10 group-focus-visible:ring-accent-green/80"><svg
-                    xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                    stroke="currentColor" aria-hidden="true" class="h-5 w-5">
-                    <path stroke-linecap="round" stroke-linejoin="round"
-                      d="M19.5 12c0-1.232-.046-2.453-.138-3.662a4.006 4.006 0 00-3.7-3.7 48.678 48.678 0 00-7.324 0 4.006 4.006 0 00-3.7 3.7c-.017.22-.032.441-.046.662M19.5 12l3-3m-3 3l-3-3m-12 3c0 1.232.046 2.453.138 3.662a4.006 4.006 0 003.7 3.7 48.656 48.656 0 007.324 0 4.006 4.006 0 003.7-3.7c.017-.22.032-.441.046-.662M4.5 12l3 3m-3-3l-3 3">
-                    </path>
-                  </svg>
+              </button>
+              <button
+                class="group flex items-center gap-1.5 p-0 transition-none disabled:cursor-not-allowed inner:transition inner:duration-200 hover:text-accent-green focus-visible:text-accent-green"><i
+                  class="relative rounded-full p-2 pt-1 not-italic group-focus-visible:ring-2 group-hover:bg-accent-green/10 group-active:bg-accent-green/20 group-focus-visible:bg-accent-green/10 group-focus-visible:ring-accent-green/80">
+                  <Icon name="material-symbols:repeat" size="18" />
                   <div
                     class="invisible absolute left-1/2 -translate-x-1/2 whitespace-nowrap rounded bg-[#666666] px-1 py-0.5 text-xs text-white opacity-0 [transition:visibility_0ms_ease_200ms,opacity_200ms_ease] dark:bg-[#495A69] group-hover:visible group-hover:opacity-100 group-hover:delay-500 group-focus-visible:visible group-focus-visible:opacity-100 translate-y-3">
-                    <span>Retweet</span>
+                    <span>Repost</span>
                   </div>
                 </i>
                 <div class="overflow-hidden"></div>
@@ -118,13 +112,8 @@
               <button
                 class="group flex items-center gap-1.5 p-0 transition-none disabled:cursor-not-allowed inner:transition inner:duration-200 hover:text-accent-pink focus-visible:text-accent-pink">
                 <i
-                  class="relative rounded-full p-2 not-italic group-focus-visible:ring-2 group-hover:bg-accent-pink/10 group-active:bg-accent-pink/20 group-focus-visible:bg-accent-pink/10 group-focus-visible:ring-accent-pink/80">
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                    stroke="currentColor" aria-hidden="true" class="h-5 w-5">
-                    <path stroke-linecap="round" stroke-linejoin="round"
-                      d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z">
-                    </path>
-                  </svg>
+                  class="relative rounded-full p-2 pt-1 not-italic group-focus-visible:ring-2 group-hover:bg-accent-pink/10 group-active:bg-accent-pink/20 group-focus-visible:bg-accent-pink/10 group-focus-visible:ring-accent-pink/80">
+                  <Icon name="icon-park-twotone:like" size="18" />
                   <div
                     class="invisible absolute left-1/2 -translate-x-1/2 whitespace-nowrap rounded bg-[#666666] px-1 py-0.5 text-xs text-white opacity-0 [transition:visibility_0ms_ease_200ms,opacity_200ms_ease] dark:bg-[#495A69] group-hover:visible group-hover:opacity-100 group-hover:delay-500 group-focus-visible:visible group-focus-visible:opacity-100 translate-y-3">
                     <span>Like</span>
@@ -132,16 +121,13 @@
                 </i>
                 <div class="overflow-hidden"></div>
               </button>
-              <button class="group flex items-center gap-1.5 p-0 transition-none disabled:cursor-not-allowed inner:transition inner:duration-200
-                                                           hover:text-accent-pink focus-visible:text-accent-pink">
+              <button
+                class="group flex items-center gap-1.5 p-0 transition-none disabled:cursor-not-allowed inner:transition inner:duration-200 hover:text-accent-pink focus-visible:text-accent-pink">
                 <i
-                  class="relative rounded-full p-2 not-italic group-focus-visible:ring-2 group-hover:bg-accent-pink/10 group-active:bg-accent-pink/20
-                                                             group-focus-visible:bg-accent-pink/10 group-focus-visible:ring-accent-pink/80">
-                  <Icon name="icon-park-twotone:share-two" />
-                  <div class="invisible absolute left-1/2 -translate-x-1/2 whitespace-nowrap rounded bg-[#666666] px-1 py-0.5 text-xs
-                                                               text-white opacity-0 [transition:visibility_0ms_ease_200ms,opacity_200ms_ease] dark:bg-[#495A69]
-                                                               group-hover:visible group-hover:opacity-100 group-hover:delay-500 group-focus-visible:visible
-                                                               group-focus-visible:opacity-100 translate-y-3">
+                  class="relative rounded-full p-2 pt-1 not-italic group-focus-visible:ring-2 group-hover:bg-accent-pink/10 group-active:bg-accent-pink/20 group-focus-visible:bg-accent-pink/10 group-focus-visible:ring-accent-pink/80">
+                  <Icon name="icon-park-twotone:share-two" size="18" />
+                  <div
+                    class="invisible absolute left-1/2 -translate-x-1/2 whitespace-nowrap rounded bg-[#666666] px-1 py-0.5 text-xs text-white opacity-0 [transition:visibility_0ms_ease_200ms,opacity_200ms_ease] dark:bg-[#495A69] group-hover:visible group-hover:opacity-100 group-hover:delay-500 group-focus-visible:visible group-focus-visible:opacity-100 translate-y-3">
                     <span>Share</span>
                   </div>
                 </i>
