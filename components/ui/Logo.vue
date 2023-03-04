61d5604f77bf740a28120869825e058f97f317b9
Albert Isern Alvarez <albert.isern.alvarez@gmail.com>
2023-03-04 22:42:03 +0100

    Improved logo size and rounded

--- components/ui/Logo.vue
+++ components/ui/Logo.vue
@@ -1,4 +1,5 @@
 <template>
   <img src="~/assets/images/Logo/LogoText.svg" alt="Logo" class="h-8 hidden 2xl:block" />
-  <img src="~/assets/images/Logo/LogoIcon.png" alt="Logo" class="h-8 w-8 2xl:hidden" />
+  <img src="~/assets/images/Logo/LogoIcon.png" alt="Logo"
+    class="h-[3rem] w-[3rem] min-w-[3rem] 2xl:hidden rounded-full" />
 </template>
