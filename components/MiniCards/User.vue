5c00526a3044e7f6a0431491dc807ea1f320126d
Albert Isern Alvarez <albert.isern.alvarez@gmail.com>
2023-03-06 00:07:49 +0100

    Improved UI (Some icons and buttons)

--- components/MiniCards/User.vue
+++ components/MiniCards/User.vue
@@ -23,20 +23,13 @@
                             </figure>
                         </NuxtLink>
                     </div>
-                    <button
-                        class="custom-button main-tab self-start border bg-light-primary px-4 py-1.5 font-bold text-white hover:bg-light-primary/90 focus-visible:bg-light-primary/90 active:bg-light-border/75 dark:bg-light-border dark:text-light-primary dark:hover:bg-light-border/90 dark:focus-visible:bg-light-border/90 dark:active:bg-light-border/75"
-                        type="button">Follow</button>
+                    <UiButtonBlack type="button" class="mt-2">Follow</UiButtonBlack>
                 </div>
                 <div>
                     <NuxtLink class="flex items-center gap-1 truncate font-bold custom-underline -mb-1 text-lg" tabindex="0"
                         :to="`/@${user.username}`">
-                        <p class="truncate">{{ user.name.first }} {{ user.name.last }}</p><i><svg
-                                xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
-                                aria-hidden="true" class="fill-accent-blue h-5 w-5">
-                                <path fill-rule="evenodd"
-                                    d="M8.603 3.799A4.49 4.49 0 0112 2.25c1.357 0 2.573.6 3.397 1.549a4.49 4.49 0 013.498 1.307 4.491 4.491 0 011.307 3.497A4.49 4.49 0 0121.75 12a4.49 4.49 0 01-1.549 3.397 4.491 4.491 0 01-1.307 3.497 4.491 4.491 0 01-3.497 1.307A4.49 4.49 0 0112 21.75a4.49 4.49 0 01-3.397-1.549 4.49 4.49 0 01-3.498-1.306 4.491 4.491 0 01-1.307-3.498A4.49 4.49 0 012.25 12c0-1.357.6-2.573 1.549-3.397a4.49 4.49 0 011.307-3.497 4.49 4.49 0 013.497-1.307zm7.007 6.387a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z"
-                                    clip-rule="evenodd"></path>
-                            </svg></i>
+                        <p class="truncate">{{ user.name.first }} {{ user.name.last }}</p>
+                        <Icon name="material-symbols:verified" class="text-main-accent h-5 w-5" />
                     </NuxtLink>
                     <div class="flex items-center gap-1 text-light-secondary dark:text-dark-secondary">
                         <NuxtLink class="truncate text-light-secondary dark:text-dark-secondary" tabindex="-1"
@@ -70,7 +63,8 @@
                         src="https://pbs.twimg.com/profile_images/1217762908882468864/n9oa2hd1_200x200.jpg" alt="" />
                 </div>
                 <div class="leading-tight">
-                    <a href="#" class="text-sm text-gray-500 hover:underline">Followed by Vue.js Developers Newsletter,
+                    <a href="#" class="text-sm font-medium text-gray-500 hover:underline">Followed by Vue.js Developers
+                        Newsletter,
                         MadeWithVueJS, and 22
                         others you follow</a>
                 </div>
