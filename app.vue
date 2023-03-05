478a0e90c84247769ebc70536dc017d919a761df
Albert Isern Alvarez <albert.isern.alvarez@gmail.com>
2023-03-06 00:02:29 +0100

    Moved sidebar to UI folder

--- app.vue
+++ app.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex w-full justify-center gap-0 lg:gap-4">
-    <HomeSidebar />
+    <Sidebar />
     <div
       class="hover-animation flex min-h-screen w-full max-w-xl flex-col border-x-0 border-dark-border dark:border-light-border xs:border-l-2 sm:border-x-2">
       <NuxtLayout>
