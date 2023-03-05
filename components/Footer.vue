7331f77e7630ce7192ac9f9907b94af89265437c
Albert Isern Alvarez <albert.isern.alvarez@gmail.com>
2023-03-06 00:03:36 +0100

    Added translations and author (Kaana Team)

--- components/Footer.vue
+++ components/Footer.vue
@@ -6,12 +6,14 @@ const app = useAppConfig() as AppConfigInput
 <template>
   <footer class="sticky top-16 flex flex-col gap-3 text-center text-sm text-light-secondary dark:text-dark-secondary">
     <nav class="flex flex-wrap justify-center gap-2">
-      <a class="custom-underline" target="_blank" rel="noreferrer" href="https://mention.earth/tos">Terms of Service</a>
-      <a class="custom-underline" target="_blank" rel="noreferrer" href="https://mention.earth/privacy">Privacy Policy</a>
-      <a class="custom-underline" target="_blank" rel="noreferrer"
-        href="https://help.kaana.io/resources/accessibility">Accessibility</a>
+      <a class="custom-underline" target="_blank" rel="noreferrer" href="https://mention.earth/tos">{{
+        $t('footer.links.terms') }}</a>
+      <a class="custom-underline" target="_blank" rel="noreferrer" href="https://mention.earth/privacy">{{
+        $t('footer.links.privacy') }}</a>
+      <a class="custom-underline" target="_blank" rel="noreferrer" href="https://help.kaana.io/resources/accessibility">{{
+        $t('footer.links.accessibility') }}</a>
     </nav>
-    <p>Made with ❤️ in the 🌎 by Kaana.</p>
+    <p>{{ $t('footer.text', { author: app.author }) }}</p>
     <p>({{ app.shortName }} {{ app.version }}).</p>
   </footer>
 </template>
