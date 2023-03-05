3de83c8aaf8233002554f32499f7ba6ea2664f76
Albert Isern Alvarez <albert.isern.alvarez@gmail.com>
2023-03-06 00:08:28 +0100

    New black button component

---
+++ components/ui/Button/Black.vue
@@ -0,0 +1,34 @@
+<template>
+    <button :class="[customClass]"
+        class="custom-button main-tab bg-light-primary px-4 py-1.5 font-bold text-white hover:bg-light-primary/90 focus-visible:bg-light-primary/90 active:bg-light-border/75 dark:bg-light-border dark:text-light-primary dark:hover:bg-dark-border/90 dark:focus-visible:bg-dark-border/90 dark:active:bg-light-border/75"
+        @click="handleClick">
+        <slot></slot>
+    </button>
+</template>
+
+<script>
+export default {
+    props: {
+        class: {
+            type: String,
+            default: ''
+        }
+    },
+    computed: {
+        customClass() {
+            return this.class
+        }
+    },
+    methods: {
+        handleClick() {
+            // Add button click logic here
+        }
+    }
+}
+</script>
+
+<style>
+button {
+    /* Add default button styles here */
+}
+</style>
