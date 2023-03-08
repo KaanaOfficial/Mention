a30b47e01c0366e8b49fa5bd90cecfc272d5a83c
Avinash verma <avinashverma165@gmail.com>
2023-03-09 05:24:40 +0530

    added keys to api

--- plugins/kanna-services.js
+++ plugins/kanna-services.js
@@ -1,12 +1,14 @@
 import Vue from 'vue';
-import kaanaServices from 'kaana-services-js';
+import kaanaServices from '@kaana/kaana-services-js';
 
 const API_KEY = process.env.API_KEY;
 const API_SECRET = process.env.API_SECRET;
+const SUPABASE_URL = process.env.SUPABASE_URL;
+const SUPABASE_KEY = process.env.SUPABASE_KEY;
 
-Vue.prototype.$kaanaServices = kaanaServices;
+Vue.prototype.kaanaServices = kaanaServices;
 
-kaanaServices.authenticate(API_KEY, API_SECRET, (err, token) => {
+kaanaServices.authenticate(API_KEY, API_SECRET, SUPABASE_KEY, SUPABASE_URL, (err, token) => {
   if (err) {
     console.error('Failed to authenticate with Kaana Auth:', err);
   } else {
